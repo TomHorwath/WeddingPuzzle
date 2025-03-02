294b4745f42bf230a3920ab03ddb0403a6fe00b1
blaise-rascal <65874622+blaise-rascal@users.noreply.github.com>
2025-03-02 16:30:56 -0500

    tweaks

--- puzzles.js
+++ puzzles.js
@@ -1,6 +1,6 @@
 const puzzles = [
-    { id: 1, title: "Gimme", image: "puzzle1_thumb.jpg", answer: "apple" },
-    { id: 2, title: "The Monkeys' Favorite", image: "puzzle2_thumb.jpg", answer: "banana" },
-    { id: 3, title: "Pacman's Favorite", image: "puzzle3_thumb.jpg", answer: "cherry" },
-    { id: 4, title: "Lindsay and I go on these", image: "puzzle4_thumb.jpg", answer: "date" }
+    { id: 1, title: "Gimme", image: "puzzle1.jpg", answer: "apple" },
+    { id: 2, title: "The Monkeys' Favorite", image: "puzzle2.jpg", answer: "banana" },
+    { id: 3, title: "Pacman's Favorite", image: "puzzle3.jpg", answer: "cherry" },
+    { id: 4, title: "Lindsay and I go on these", image: "puzzle4.jpg", answer: "date" }
 ];
